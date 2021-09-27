f42a4dff2179d3f311f28d6027b632ef647cc2de
Sreejith <L00163270@student.lyit.ie>
2021-09-27 12:19:42 +0530

    first commit

---
+++ tut1/q2.py
@@ -0,0 +1,17 @@
+top_icon = "A"
+bottom_icon = "v"
+left_icon = "<"
+right_icon = ">"
+
+print (top_icon *48)
+print (left_icon + "\t"*12 + right_icon)
+print (left_icon + "\t"*12 + right_icon)
+print (left_icon + "\t"*12 + right_icon)
+print (left_icon + "\t"*5 + "Title" +_"\t" * 6 + right_icon)
+print (left_icon + "\t"*12 + right_icon)
+print (left_icon + "\t"*12 + right_icon)
+print (left_icon + "\t" + "A \t Option a " +_"\t" * 8 + right_icon)
+print (left_icon + "\t" + "A \t Option a " +_"\t" * 8 + right_icon)
+print (left_icon + "\t" + "A \t Option a " +_"\t" * 8 + right_icon)
+
+
---
+++ tut1/q3.py
@@ -0,0 +1,13 @@
+
+ppsn # Unique number needed to idnetify as
+passport number # Id to adress yourself in another country
+temparature in degrees # degress will be undertood by all
+cao number #
+Miles travel per day # min 2 miles is good for healthy
+Average collection of exam grades # to get good understanding for your overall grade
+File path # system need to run with exact exe
+Persons middle initial # not a mandatory
+Biography statemnt # to understand about ourself
+Book abstract # to understand about the book before reading to start
+Mathematical function # to get correct calculations
+
